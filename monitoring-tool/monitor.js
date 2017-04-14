9ef6719844636e95ecb126ba30d2c3b0332c6222
pkulkar5 <pkulkar5@ncsu.edu>
2017-04-14 14:56:58 -0400

    added key exists checks

--- monitoring-tool/monitor.js
+++ monitoring-tool/monitor.js
@@ -5,37 +5,50 @@ var client = redis.createClient(6379, '127.0.0.1', {});
 
 setInterval(function() {
 
-    client.lrange("prodServers", 0, -1, function(err, prodServers) {
-        if(err) throw err;
+    client.exists("prodServers", function(exception, existsValue) {
+        if(existsValue != 0) {
+            client.exists("prodUrls", function(exc, existsVal) {
+                if(existsVal != 0) {
 
-        for(var i = 0; i < prodServers.length; i++) {
-            var errCount =0;
-            client.lrange("prodUrls", 0, -1, function(e, prodUrls) {
-                if(e) throw e
-                for(var j = 0; j < prodUrls.length; j++) {
-                    var url = "http://" + prodServers[i] + prodUrls[j];
-                    request(url, function(er, x, y) {
-                        if(er) throw er;
-                        if(x.statusCode === 500 || x.statusCode === 404) {
-                            errCount +=1;
+                    client.lrange("prodServers", 0, -1, function(err, prodServers) {
+                        if(err) throw err;
+
+                        for(var i = 0; i < prodServers.length; i++) {
+                            var errCount =0;
+                            client.lrange("prodUrls", 0, -1, function(e, prodUrls) {
+                                if(e) throw e
+                                for(var j = 0; j < prodUrls.length; j++) {
+                                    var url = "http://" + prodServers[i] + prodUrls[j];
+                                    request(url, function(er, x, y) {
+                                        if(er) throw er;
+                                        if(x.statusCode === 500 || x.statusCode === 404) {
+                                            errCount +=1;
+                                        }
+                                    });
+                                }
+
+                                client.llen("prodUrls", function(er, numOfUrls){
+                                    if(er) throw er;
+
+                                    if(errCount === numOfUrls){
+                                        client.lrem("prodServers", 0, prodServers[i], function(error,z) {
+                                            if(error) throw error;
+                                            console.log("Stopped routing to Prod Server" + prodServers[i]);
+                                        })
+                                    }
+                                })
+
+                            });
                         }
                     });
+
                 }
-
-                client.llen("prodUrls", function(er, numOfUrls){
-                    if(er) throw er;
-
-                    if(errCount === numOfUrls){
-                        client.lrem("prodServers", 0, prodServers[i], function(error,z) {
-                            if(error) throw error;
-                            console.log("Stopped routing to Prod Server" + prodServers[i]);
-                        })
-                    }
-                })
-
-            });
+            })
         }
-    });
+
+    })
+
+
 
 
 }, 10000);
@@ -44,26 +57,35 @@ setInterval(function() {
 
 setInterval(function() {
 
-    client.lrange("canServers", 0, -1, function(err, canServers) {
-        if(err) throw err;
-        for(var i = 0; i < canServers.length; i++) {
-            client.lrange("canUrls", 0, -1, function(e, canUrls) {
-                if(e) throw e;
-                for(var j = 0; j < canUrls.length; j++) {
-                    var url = "http://" + canServers[i] + canUrls[j];
-                    request(url, function(er, x, y) {
-                        if(er) throw er;
-                        if(x.statusCode === 500) {
-                            client.set("trafficRatio", 1.0, function(error, val) {
-                                if(error) throw error;
-                                console.log("Stopped routing to Canary Server");
+    client.exists("canServers", function(exception, existsValue) {
+        if(existsValue != 0) {
+            client.exists("canUrls", function(exc, existsVal) {
+                if(existsVal != 0) {
+
+                    client.lrange("canServers", 0, -1, function(err, canServers) {
+                        if(err) throw err;
+                        for(var i = 0; i < canServers.length; i++) {
+                            client.lrange("canUrls", 0, -1, function(e, canUrls) {
+                                if(e) throw e;
+                                for(var j = 0; j < canUrls.length; j++) {
+                                    var url = "http://" + canServers[i] + canUrls[j];
+                                    request(url, function(er, x, y) {
+                                        if(er) throw er;
+                                        if(x.statusCode === 500) {
+                                            client.set("trafficRatio", 1.0, function(error, val) {
+                                                if(error) throw error;
+                                                console.log("Stopped routing to Canary Server");
+                                            });
+                                        }
+                                    });
+                                }
                             });
                         }
                     });
-                }
-            });
-        }
-    });
 
+                }
+            })
+        }
+    })
 
 }, 15000);
