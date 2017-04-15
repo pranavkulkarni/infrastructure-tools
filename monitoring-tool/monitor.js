853c9e93a60f54cdc0c634853b696c2aba9f9acd
pkulkar5 <pkulkar5@ncsu.edu>
2017-04-14 20:55:48 -0400

    reverted port no

--- monitoring-tool/monitor.js
+++ monitoring-tool/monitor.js
@@ -18,7 +18,8 @@ setInterval(function() {
                             client.lrange("prodUrls", 0, -1, function(e, prodUrls) {
                                 if(e) throw e
                                 for(var j = 0; j < prodUrls.length; j++) {
-                                    var url = "http://" + prodServers[i] + ":" + apiPortNo + prodUrls[j];
+                                    //var url = "http://" + prodServers[i] + ":" + apiPortNo + prodUrls[j];
+                                    var url = "http://" + prodServers[i] + prodUrls[j];
                                     request(url, function(er, x, y) {
                                         if(er) throw er;
                                         if(x.statusCode === 500) {
@@ -68,7 +69,8 @@ setInterval(function() {
                             client.lrange("canUrls", 0, -1, function(e, canUrls) {
                                 if(e) throw e;
                                 for(var j = 0; j < canUrls.length; j++) {
-                                    var url = "http://" + canServers[i] + ":" + apiPortNo + canUrls[j];
+                                    //var url = "http://" + canServers[i] + ":" + apiPortNo + canUrls[j];
+                                    var url = "http://" + canServers[i] + canUrls[j];
                                     request(url, function(er, x, y) {
                                         if(er) throw er;
                                         if(x.statusCode === 500) {
